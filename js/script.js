f7b40c444852131efbfb08b7117d60449f013037
Stephanie <stephanie.furlan94@gmail.com>
2020-11-10 17:59:17 +0100

    function empty

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+function empty(s) {
+   return s.replace(/\s/g, "");
+}
+
 var nome = prompt("Qual'è il tuo nome?");
-while (nome.length == 0) {
+while (empty(nome).length == 0) {
     nome = prompt("Inserisci il tuo NOME!!!!");
 }
 
 var cognome = prompt("Qual'è il tuo cognome?");
-while (cognome.length == 0) {
+while (empty(cognome).length == 0) {
    cognome = prompt("Inserisci il tuo COGNOME!!!!");
 }
 
@@ -13,13 +17,12 @@ var colore = prompt("Qual'è il tuo colore preferito?");
 // DEFAULT - random from array //
 var array = ["Blue", "Verde", "Rosso", "Magenta Chiaro"];
 
-if (colore.length == 0) {
-   console.log("hello");
+if (empty(colore).length == 0) {
    colore = array[Math.floor(Math.random() * array.length)];
 }
 
 document.getElementById("nome").innerHTML = nome;
 
 // Remove empty spaces //
-var password = nome.replace(/\s/g, "") + cognome.replace(/\s/g, "") + colore.replace(/\s/g, "") + "19";
+var password = empty(nome) + empty(cognome) + empty(colore) + "19";
 document.getElementById("password").innerHTML = password;
