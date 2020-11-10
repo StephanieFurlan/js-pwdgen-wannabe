c296c59b165a67e5b95ec53fe47f763bddbc5b97
Stephanie <stephanie.furlan94@gmail.com>
2020-11-10 17:44:09 +0100

    Prompt validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
-var nome = prompt("Qual'è il tuo nome?");
-var cognome = prompt("Qual'è il tuo cognome?");
-var colore = prompt("Qual'è il tuo colore preferito?");
+function removeEmpty(string) {
+  return string.replace(/\s+/g, '');
+}
 
+var nome = prompt("Qual'è il tuo nome?");
+while (nome.length == 0) {
+    nome = prompt("Inserisci il tuo NOME!!!!");
+}
+
+var cognome = prompt("Qual'è il tuo cognome?");
+while (cognome.length == 0) {
+   cognome = prompt("Inserisci il tuo COGNOME!!!!");
+}
+
+var colore = prompt("Qual'è il tuo colore preferito?");
+// DEFAULT - ROSSO //
+if (colore.length == 0) {
+   colore = "Rosso";
+}
 
 document.getElementById("nome").innerHTML = nome;
-var password = nome + cognome + colore + "19";
+
+// Remove empty spaces //
+
+
+var password = removeEmpty(nome) + removeEmpty(cognome) + removeEmpty(colore) + "19";
 document.getElementById("password").innerHTML = password;
